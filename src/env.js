7c0534555044327d72a492fd2d2b68f6fcbf374d
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-09-24 17:15:50 -0700

    API_BASE_URL

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
+  process.env.REACT_APP_API_BASE_URL || 'https://beer-me-back-end.herokuapp.com'
 
 exports.default = API_BASE_URL
